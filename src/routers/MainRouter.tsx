2e9a4e982e485eab81874b96f60f806f7335a85a
silvatarik <silvatarikt@gmail.com>
2021-10-19 14:13:12 +0100

    update minor files

--- src/routers/MainRouter.tsx
+++ src/routers/MainRouter.tsx
@@ -1,8 +1,34 @@
-import React from "react";
-import { BrowserRouter, Route, Switch, RouteComponentProps } from 'react-router-dom'
+import { onAuthStateChanged } from "@firebase/auth";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  RouteComponentProps,
+} from "react-router-dom";
+import { login } from "../actions/auth/authActions";
+import { auth } from "../config/firebase";
 import routes from "../config/routes";
 
 const MainRouter: React.FunctionComponent<{}> = (props) => {
+
+  const dispatch = useDispatch();
+  const [isAunthenthicate, setisAunthenthicate] = useState<boolean>(false);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user?.uid) {
+        if (user.uid != null && user.displayName != null) {
+            dispatch(login(user.uid, user.displayName));
+            setisAunthenthicate(true);
+        }
+      } else {
+        setisAunthenthicate(false);
+      }
+    });
+  }, [auth, dispatch, setisAunthenthicate]);
+
   return (
     <BrowserRouter>
       <Switch>
